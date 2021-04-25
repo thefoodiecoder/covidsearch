821df33fa55d02251ce3e624c6011e181f08ff7c
Vijay Chhuttani <vijay@zoop.one>
2021-04-25 18:32:35 +0530

    Add search query builder constants

--- constants/providings.js
+++ constants/providings.js
@@ -1,11 +1,10 @@
 const providings = {
-    BED_WITH_OXYGEN: "Bed with oxygen",
     BED: "Bed without oxygen",
     OXYGEN: "Oxygen",
     PLASMA: "Plasma",
     AMBULANCE: "Ambulance",
     FOOD: "Food Services",
-    MEDICINE: "Medicines"
+    MEDICINE: "Medicines",
 };
 
 export default providings;
